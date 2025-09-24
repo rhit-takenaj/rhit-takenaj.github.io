97f58100a71d77895c65fac8a89c08bc929a6ef3
takenaj <takenaj@rose-hulman.edu>
2025-09-24 09:44:13 -0400

    added functionality to dark theme button

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,34 @@ portfolio_button.addEventListener("click", function() {
     changeWebPage("portfolio");
 });
 
+const dark_theme = document.getElementsByClassName("dark_theme")[0];
+dark_theme.addEventListener("click", function() {
+    toggleDarkTheme()
+});
+
 function changeWebPage(page) {
     window.location.assign(page + ".html")
+}
+
+function toggleDarkTheme(cur_theme) {
+    let html = document.getElementById("html");
+    let theme = html.classList[0];
+    if(theme === "dark") {
+        html.classList.remove(html.classList[0]);
+        html.classList.add("light");
+        dark_theme.innerHTML = "Light Theme";
+    } else {
+        if(theme === "light") {
+            html.classList.remove(html.classList[0]);
+            html.classList.add("dark");
+            dark_theme.innerHTML = "Dark Theme";
+        } else {
+            html.classList.remove(html.classList[0]);
+            html.classList.add("light");
+            dark_theme.innerHTML = "Light Theme";
+        }
+    }
+
 }
 
 //nav buttons from https://www.w3schools.com/js/js_window_location.asp
