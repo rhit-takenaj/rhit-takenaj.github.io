db4263a02094796512552eb86ebad05534c72a54
takenaj <takenaj@rose-hulman.edu>
2025-09-24 09:01:12 -0400

    started js

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const home_button = document.getElementsByClassName("home_button")[0];
+home_button.addEventListener("click", function() {
+    changeWebPage("home");
+});
+
+const resume_button = document.getElementsByClassName("resume_button")[0];
+resume_button.addEventListener("click", function() {
+    changeWebPage("resume");
+});
+
+const portfolio_button = document.getElementsByClassName("portfolio_button")[0];
+portfolio_button.addEventListener("click", function() {
+    changeWebPage("portfolio");
+});
+
+function changeWebPage(page) {
+    location.href = page + ".html";
+}
